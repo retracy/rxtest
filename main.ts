1b02df99367e688a17634d82b5ca9aae7cc601c8
Bob Tracy <retracy@live.com>
2018-01-12 15:50:20 -0800

    Added extra debug logging

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ var numbers = [ 1, 2, 3, 4 ];
 
 var source = Observable.from(numbers);
 
-source.reduce((x, y) => x + y).subscribe(
+source.reduce((x, y) => {
+    console.log(`x is ${x}, y is ${y}`);
+    return x + y;
+}, 0).subscribe(
     value => console.log(`final value is ${value}`)
 );
