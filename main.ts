e0d7c731b20109e966c693f118b2e805cddef1f9
Bob Tracy <retracy@live.com>
2018-01-12 16:38:33 -0800

    Use objects instead of numbers

--- main.ts
+++ main.ts
@@ -1,12 +1,36 @@
 import { Observable } from "rxjs";
 
-var numbers = [ 1, 2, 3, 4 ];
+var edits = [
+    {
+        id: 1,
+        name: "Betty"
+    },
+    {
+        id: 2,
+        name: "Ringo"
+    },
+    {
+        id: 3,
+        name: "Steve"
+    }
+];
 
-var source = Observable.from(numbers);
+var source: Observable<{ id, name }> = Observable.from(edits);
 
-source.reduce((x, y) => {
-    console.log(`x is ${x}, y is ${y}`);
-    return x + y;
-}, 0).subscribe(
-    value => console.log(`final value is ${value}`)
-);
+function dump({ count, names }) {
+    console.log(`count is ${count}`);
+    for (var item of names) {
+        console.log(item);
+    }
+}
+source.reduce((acc, value) => {
+    // id is ignored
+    acc.count += 1;
+    acc.names.push(value.name);
+    dump(acc);
+    return acc;
+}, { count: 0, names: [] }).subscribe(
+    value => {
+        console.log('final value');
+        dump(value);
+    });
